33d752b01e1782c21d9037c64932ac709db71ba9
Jack Rothrock <jrothrock14@gmail.com>
2023-10-14 23:09:39 -0600

    Remove asset pipeline

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   config.active_record.verbose_query_logs = true
 
   # Suppress logger output for asset requests.
-  config.assets.quiet = true
+  # config.assets.quiet = true
 
   # Default url for devise mailers.
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
